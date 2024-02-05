6cb59f0ff6e3f2684a6bb9787d2753c533237dab
darwin-morocho <darwin.morocho@icloud.com>
2024-02-05 13:22:50 -0500

    init version

--- lib/domain/entities/company.dart
+++ lib/domain/entities/company.dart
@@ -2,10 +2,8 @@ class Company {
   Company({
     required this.id,
     required this.name,
-    required this.description,
   });
 
   final int id;
   final String name;
-  final String? description;
 }
